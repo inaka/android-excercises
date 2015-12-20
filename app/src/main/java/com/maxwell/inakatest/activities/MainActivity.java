fda8d78558c73b353db132aeda38f9de2bf589f7
Maxwell <maxib2007@gmail.com>
2015-12-20 13:28:35 -0300

    implementing libs

--- app/src/main/java/com/maxwell/inakatest/activities/MainActivity.java
+++ app/src/main/java/com/maxwell/inakatest/activities/MainActivity.java
@@ -1,4 +1,4 @@
-package com.maxwell.inakatest;
+package com.maxwell.inakatest.activities;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.maxwell.inakatest.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/maxwell/inakatest/loaders/dummy.java
@@ -0,0 +1,7 @@
+package com.maxwell.inakatest.loaders;
+
+/**
+ * Created by Maxwell on 20/12/2015.
+ */
+public class dummy {
+}
